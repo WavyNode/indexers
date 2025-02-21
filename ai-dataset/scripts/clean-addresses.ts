96116d96ac80534c1228cb321ee1ba7fa9c15645
ede-404 <eder077@proton.me>
2025-02-21 10:07:34 -0600

    [ADD] get block addresses

---
+++ ai-dataset/scripts/clean-addresses.ts
@@ -0,0 +1,31 @@
+import { ethers } from "ethers";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+
+async function getLatestBlock() {
+    
+    const latestBlockNumber = await provider.getBlockNumber();
+    const block = await provider.getBlockWithTransactions(latestBlockNumber);
+
+    console.log (`xD bloque actual: ${block.number}`)
+    return block;
+}
+
+async function getAddressesFromBlock() {
+    const block = await getLatestBlock();
+    if (!block || !block.transactions || block.transactions.length === 0) return[];
+
+    const validTransactions= block.transactions.filter(tx => tx?.from && tx?.to);
+
+    const first10Addresses = validTransactions.slice(0, 20).map(tx =>({
+        form:tx.from,
+        to:tx.to
+    }));
+    
+    console.log("Primeras transaciones con from y to:", first10Addresses);
+    return first10Addresses;    
+}
+getAddressesFromBlock();
